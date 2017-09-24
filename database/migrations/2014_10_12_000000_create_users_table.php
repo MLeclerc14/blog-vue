7e14049eeb3d34d6bf8e9f2f8caff501b66c4513
Maxime Leclerc <contact@maxime-leclerc.com>
2017-09-24 15:09:46 +0200

    feat(auth): users authentication

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->boolean('active')->default(0);
+            $table->string('username')->unique();
+            $table->string('firstname');
+            $table->string('lastname');
             $table->string('email')->unique();
             $table->string('password');
+            $table->datetime('lastLogin')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
